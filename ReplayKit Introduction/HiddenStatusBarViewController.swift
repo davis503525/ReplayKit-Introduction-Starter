769dc377aaf4eba615825de1258f3011f13874ee
Davis Allie <davis525@live.com.au>
2015-12-06 17:26:42 +1100

    Extra Files

---
+++ ReplayKit Introduction/HiddenStatusBarViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HiddenStatusBarViewController.swift
+//  ReplayKit Introduction
+//
+//  Created by Davis Allie on 6/12/2015.
+//  Copyright © 2015 tutsplus. All rights reserved.
+//
+
+import UIKit
+
+class HiddenStatusBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
